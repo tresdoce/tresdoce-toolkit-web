79d214a3ddace565dbb5648340882e58df1e36fc
Maximiliano Delgado <mdelgado@baufest.com>
2021-03-10 23:53:13 -0300

    chore: first commit

---
+++ packages/babel-config/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    presets: ['@babel/preset-typescript', '@babel/preset-react'],
+    plugins: [
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-proposal-class-properties',
+    ],
+};
